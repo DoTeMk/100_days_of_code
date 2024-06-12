325d5d766b6513065321dac66c25174df81e21c7
DoTeMk <douglasmenezes167@gmail.com>
2024-06-11 21:36:06 -0300

    2

--- day2/tip_calculator.py
+++ day2/tip_calculator.py
@@ -6,4 +6,4 @@ tip_percentage = int(input("How much tip would you like to give? "))
 split_number = int(input("How many people to split the bill? "))
 
 bill_splited = (total_bill + (total_bill * (tip_percentage / 100))) / split_number
-print(f"Each person should pay: ${bill_splited:.2f}")
+print(f"Each person should pay: ${bill_splited:.2f}") 
