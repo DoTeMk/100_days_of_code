35b5e8ca1c89e7e1c579280d19480ddf764dea06
DoTeMk <douglasmenezes167@gmail.com>
2024-06-17 10:32:20 -0300

    Updated Reeborgs

--- day6/escaping_the_maze.py
+++ day6/escaping_the_maze.py
@@ -7,20 +7,14 @@
 
 
 
-# def turn_right():
-#     turn_left()
-#     turn_left()
-#     turn_left()
-    
 # while front_is_clear():
 #     move()
+# turn_left()
     
     
 # while not at_goal():
-#     if not wall_on_right():
+#     if right_is_clear():
 #         turn_right()
-#         move()
-#     if wall_on_left():
 #         move()
 #     elif front_is_clear():
 #         move()
