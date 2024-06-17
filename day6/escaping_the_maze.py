71c74874585ab03aaca8549076667407ff69eee7
DoTeMk <douglasmenezes167@gmail.com>
2024-06-17 10:25:30 -0300

    Reeborg's Challenge 17_06_24

---
+++ day6/escaping_the_maze.py
@@ -0,0 +1,28 @@
+"""
+    Code for use in: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+    ("Reeborg's World Challenge")
+"""
+
+
+
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+    
+    
+# while not at_goal():
+#     if not wall_on_right():
+#         turn_right()
+#         move()
+#     if wall_on_left():
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
